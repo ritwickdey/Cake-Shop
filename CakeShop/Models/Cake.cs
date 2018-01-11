d10a7d66ce7f4f082ef36e86f2f7140025787189
Ritwick Dey <ritwickdey@outlook.com>
2018-01-12 00:21:33 +0530

    model added

---
+++ CakeShop/Models/Cake.cs
@@ -0,0 +1,19 @@
+﻿namespace CakeShop.Models
+{
+    public class Cake
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public string AllergyInfo { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
+        public bool IsCakeOfTheWeek { get; set; }
+        public bool InStock { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ CakeShop/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace CakeShop.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<Cake> Cakes { get; set; }
+
+        public Category()
+        {
+            Cakes = new Collection<Cake>();
+        }
+    }
+}
