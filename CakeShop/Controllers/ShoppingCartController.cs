80a9aa31e1022e0c070999d68435c0c67e3997eb
Ritwick Dey <ritwickdey@outlook.com>
2018-01-13 00:31:15 +0530

    Font Awesome Package added

---
+++ CakeShop/Controllers/ShoppingCartController.cs
@@ -0,0 +1,22 @@
+﻿using CakeShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CakeShop.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly ICakeRepository _cakeRepository;
+        private readonly IShoppingCart _shoppingCart;
+
+        public ShoppingCartController(ICakeRepository cakeRepository, IShoppingCart shoppingCart)
+        {
+            _cakeRepository = cakeRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- CakeShop/wwwroot/js/main.js
+++ CakeShop/wwwroot/js/main.js
@@ -1,3 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'jquery/dist/jquery.js';
+import 'font-awesome/css/font-awesome.css';
+
 import '../css/Style.css';
