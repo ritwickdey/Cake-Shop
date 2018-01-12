204005abc43dde56d69541614b1d6dd0b62778cc
Ritwick Dey <ritwickdey@outlook.com>
2018-01-12 21:51:56 +0530

    tagHelper added

---
+++ CakeShop/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CakeShop.TagHelpers
+{
+    [HtmlTargetElement("email")]
+    public class EmailTagHelper : TagHelper
+    {
+        public string Address { get; set; }
+        public string Content { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", $"mailto:{Address}");
+            output.Content.SetContent(Content);
+        }
+    }
+}
