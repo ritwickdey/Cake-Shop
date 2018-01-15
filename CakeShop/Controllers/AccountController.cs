5653051881a78f7615148008792264ab5df5b39f
Ritwick Dey <ritwickdey@outlook.com>
2018-01-15 15:24:03 +0530

    Login functionality

---
+++ CakeShop/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using CakeShop.Core.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CakeShop.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login([FromQuery]string returnUrl)
+        {
+            return View(new LoginViewModel
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromForm]LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginViewModel);
+            }
+
+            var user = await _userManager.FindByEmailAsync(loginViewModel.Email);
+            if (user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+                if (result.Succeeded)
+                {
+                    return Redirect(loginViewModel.ReturnUrl ?? "/");
+                }
+            }
+
+            ModelState.AddModelError("", "Invalid Username or Password");
+            return View(loginViewModel);
+        }
+
+
+    }
+}
---
+++ CakeShop/Core/ViewModel/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CakeShop.Core.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
